0f80ce6af312d539717bc0b4497a3905cd4de2c5
TheMuffinmuffler <willih@stud.ntnu.no>
2026-02-20 14:47:21 +0100

    Working ArUco scanner, should in theory calculate the distance from the robot as it knows the size off the codes.

--- src/ris/scripts/aruco_mapper.py
+++ src/ris/scripts/aruco_mapper.py
@@ -6,7 +6,7 @@ import rospy
 import cv2 as cv
 import numpy as np
 import cv2.aruco as aruco
-from sensor_msgs.msg import CompressedImage, CameraInfo
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 
 class ArucoMapper:
@@ -14,30 +14,26 @@ class ArucoMapper:
         self.camera_name = rospy.get_param("~camera_name","csi_cam_0")
         self.bridge = CvBridge()
 
-        # --- Start empty, waiting for your new calibration data! ---
-        self.camera_matrix = None
-        self.dist_coeffs = None
+        # --- YOUR CUSTOM CALIBRATION MATH ---
+        # Matrix 'K' from your calibration
+        self.camera_matrix = np.array([
+            [399.340134, 0.000000, 324.141394],
+            [0.000000, 532.852065, 217.404681],
+            [0.000000, 0.000000, 1.000000]
+        ])
 
-        # IMPORTANT: Make sure this is the exact size of your printed ArUco marker in meters!
+        # Distortion 'D' from your calibration
+        self.dist_coeffs = np.array([[-0.322724, 0.097739, -0.000331, -0.001090, 0.0]])
+
+        # Your measured ArUco marker size in meters
         self.marker_size = 0.038
 
-        # Listen to BOTH the video feed and the new calibration info
+        # Only listen to the video feed now, we already have the math!
         self.image_sub = rospy.Subscriber(self.camera_name+"/image_raw/compressed", CompressedImage, self.callback)
-        self.info_sub = rospy.Subscriber(self.camera_name+"/camera_info", CameraInfo, self.info_callback)
-
         self.image_pub = rospy.Publisher("/aruco_video/compressed", CompressedImage, queue_size=10)
 
         self.aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
         self.aruco_params = aruco.DetectorParameters_create()
-
-    # --- Listen for the math you just generated ---
-    def info_callback(self, msg):
-        # Once we receive a valid matrix (K[0] is not 0), save it and stop listening
-        if self.camera_matrix is None and msg.K[0] != 0.0:
-            self.camera_matrix = np.array(msg.K).reshape((3,3))
-            self.dist_coeffs = np.array(msg.D)
-            print("\n--- SUCCESS: Custom Camera Calibration Received! ---\n")
-            self.info_sub.unregister()
 
     def callback(self, data):
         try:
@@ -52,22 +48,19 @@ class ArucoMapper:
         if ids is not None:
             aruco.drawDetectedMarkers(img, corners, ids)
 
-            # Only do 3D math if we have received your custom calibration
-            if self.camera_matrix is not None:
-                rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(
-                    corners, self.marker_size, self.camera_matrix, self.dist_coeffs)
+            # Calculate the 3D distance using your custom lens math!
+            rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(
+                corners, self.marker_size, self.camera_matrix, self.dist_coeffs)
 
-                for i in range(len(ids)):
-                    aruco.drawAxis(img, self.camera_matrix, self.dist_coeffs, rvecs[i], tvecs[i], self.marker_size)
+            for i in range(len(ids)):
+                aruco.drawAxis(img, self.camera_matrix, self.dist_coeffs, rvecs[i], tvecs[i], self.marker_size)
 
-                    x_dist = tvecs[i][0][0]
-                    y_dist = tvecs[i][0][1]
-                    z_dist = tvecs[i][0][2]
+                x_dist = tvecs[i][0][0]
+                y_dist = tvecs[i][0][1]
+                z_dist = tvecs[i][0][2]
 
-                    print("Marker [{}]: is {:.2f}m left/right, {:.2f}m up/down, and {:.2f}m straight ahead!".format(
-                        ids[i][0], x_dist, y_dist, z_dist))
-            else:
-                print("Waiting for your custom Camera Calibration...")
+                print("Marker [{}]: is {:.2f}m left/right, {:.2f}m up/down, and {:.2f}m straight ahead!".format(
+                    ids[i][0], x_dist, y_dist, z_dist))
 
         cv.imshow("ArUco Scanner", img)
         cv.waitKey(3)
