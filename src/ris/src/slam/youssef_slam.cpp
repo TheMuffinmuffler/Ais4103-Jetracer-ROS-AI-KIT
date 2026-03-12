15885c4e7c8d6f289fa8868ca1ebe8a59d15bc14
youssefghafour <youssef.a.ghafour@gmail.com>
2026-03-13 00:10:48 +0100

    updating

--- src/ris/src/slam/youssef_slam.cpp
+++ src/ris/src/slam/youssef_slam.cpp
@@ -1,57 +1,50 @@
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
-#include <std_msgs/Empty.h>
 #include <geometry_msgs/Twist.h>
 
-#include <thread>
-#include <atomic>
 #include <cstdlib>
 #include <sstream>
 #include <string>
 
-class SlamManager
+class YoussefSlam
 {
 public:
-    SlamManager()
+    YoussefSlam()
         : nh_(), pnh_("~"),
           has_map_(false),
+          have_previous_map_(false),
           save_started_(false),
           finished_(false),
           stable_time_acc_(0.0)
     {
         pnh_.param("map_topic", map_topic_, std::string("/map"));
-        pnh_.param("save_trigger_topic", save_trigger_topic_, std::string("/slam/save_request"));
         pnh_.param("laptop_map_dir", laptop_map_dir_, std::string("/home/jetson/laptop_slam_maps"));
         pnh_.param("laptop_map_name", laptop_map_name_, std::string("mymap"));
+        pnh_.param("robot_map_dir", robot_map_dir_,
+                   std::string("/home/jetson/catkin_ws/src/maps"));
+        pnh_.param("robot_map_name", robot_map_name_, std::string("mymap"));
         pnh_.param("check_period_sec", check_period_sec_, 2.0);
         pnh_.param("stable_seconds_before_save", stable_seconds_before_save_, 8.0);
         pnh_.param("changed_cells_threshold", changed_cells_threshold_, 20);
-        pnh_.param("launch_gmapping", launch_gmapping_, false);
-        pnh_.param("gmapping_launch_cmd", gmapping_launch_cmd_,
-                   std::string("roslaunch jetracer gmapping.launch"));
         pnh_.param("gmapping_node_name", gmapping_node_name_, std::string("/slam_gmapping"));
         pnh_.param("stop_robot_before_save", stop_robot_before_save_, true);
 
-        map_sub_ = nh_.subscribe(map_topic_, 1, &SlamManager::mapCallback, this);
-        save_pub_ = nh_.advertise<std_msgs::Empty>(save_trigger_topic_, 1, true);
+        map_sub_ = nh_.subscribe(map_topic_, 1, &YoussefSlam::mapCallback, this);
         cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
 
-        timer_ = nh_.createTimer(ros::Duration(check_period_sec_), &SlamManager::timerCallback, this);
+        timer_ = nh_.createTimer(ros::Duration(check_period_sec_), &YoussefSlam::timerCallback, this);
 
-        std::stringstream mkdir_cmd;
-        mkdir_cmd << "mkdir -p " << laptop_map_dir_;
-        std::system(mkdir_cmd.str().c_str());
+        std::stringstream mkdir_robot_cmd;
+        mkdir_robot_cmd << "mkdir -p " << robot_map_dir_;
+        std::system(mkdir_robot_cmd.str().c_str());
 
-        if (launch_gmapping_)
-        {
-            ROS_INFO_STREAM("Launching gmapping with command: " << gmapping_launch_cmd_);
-            gmapping_thread_ = std::thread([this]() {
-                std::system(gmapping_launch_cmd_.c_str());
-            });
-            gmapping_thread_.detach();
-        }
+        std::stringstream mkdir_laptop_cmd;
+        mkdir_laptop_cmd << "mkdir -p " << laptop_map_dir_;
+        std::system(mkdir_laptop_cmd.str().c_str());
 
-        ROS_INFO("slam_manager started.");
+        ROS_INFO("youssef_slam started.");
+        ROS_INFO_STREAM("Robot maps will be saved to: " << robot_map_dir_);
+        ROS_INFO_STREAM("Laptop maps will be saved to: " << laptop_map_dir_);
     }
 
 private:
@@ -78,20 +71,29 @@ private:
     void publishStop()
     {
         geometry_msgs::Twist zero;
-        for (int i = 0; i < 5; ++i)
+        for (int i = 0; i < 10; ++i)
         {
             cmd_pub_.publish(zero);
             ros::Duration(0.05).sleep();
         }
     }
 
-    void triggerRobotSave()
+    void saveRobotMap()
     {
-        std_msgs::Empty msg;
-        for (int i = 0; i < 3; ++i)
+        std::stringstream ss;
+        ss << "mkdir -p " << robot_map_dir_
+           << " && rosrun map_server map_saver -f "
+           << robot_map_dir_ << "/" << robot_map_name_;
+
+        ROS_INFO_STREAM("Saving robot map with command: " << ss.str());
+        int ret = std::system(ss.str().c_str());
+        if (ret != 0)
         {
-            save_pub_.publish(msg);
-            ros::Duration(0.2).sleep();
+            ROS_WARN_STREAM("Robot map save command returned non-zero code: " << ret);
+        }
+        else
+        {
+            ROS_INFO("Robot map saved successfully.");
         }
     }
 
@@ -107,6 +109,10 @@ private:
         if (ret != 0)
         {
             ROS_WARN_STREAM("Laptop map save command returned non-zero code: " << ret);
+        }
+        else
+        {
+            ROS_INFO("Laptop map saved successfully.");
         }
     }
 
@@ -130,7 +136,7 @@ private:
         if (stop_robot_before_save_)
             publishStop();
 
-        triggerRobotSave();
+        saveRobotMap();
         saveLaptopMap();
         stopGmapping();
 
@@ -180,7 +186,6 @@ private:
     ros::NodeHandle pnh_;
 
     ros::Subscriber map_sub_;
-    ros::Publisher save_pub_;
     ros::Publisher cmd_pub_;
     ros::Timer timer_;
 
@@ -188,7 +193,7 @@ private:
     nav_msgs::OccupancyGrid previous_map_;
 
     bool has_map_;
-    bool have_previous_map_ = false;
+    bool have_previous_map_;
     bool save_started_;
     bool finished_;
 
@@ -196,24 +201,20 @@ private:
     double check_period_sec_;
     double stable_seconds_before_save_;
     int changed_cells_threshold_;
-
-    bool launch_gmapping_;
     bool stop_robot_before_save_;
-    std::string gmapping_launch_cmd_;
-    std::string gmapping_node_name_;
 
     std::string map_topic_;
-    std::string save_trigger_topic_;
     std::string laptop_map_dir_;
     std::string laptop_map_name_;
-
-    std::thread gmapping_thread_;
+    std::string robot_map_dir_;
+    std::string robot_map_name_;
+    std::string gmapping_node_name_;
 };
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "slam_manager");
-    SlamManager manager;
+    ros::init(argc, argv, "youssef_slam");
+    YoussefSlam node;
     ros::spin();
     return 0;
 }
